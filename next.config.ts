49ae70f7c8852cb1e9286d6c303eb999c22337f7
lishun <silencewood@163.com>
2025-10-05 10:35:05 +0800

    feat: 生产的预渲染

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  /** 增量渲染，需要配合页面或者路由中加上export const experimental_ppr = true */
+  experimental: {
+    ppr: 'incremental'
+  }
 };
 
 export default nextConfig;
