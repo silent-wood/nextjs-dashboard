96f2f18110c08e81b96ecc8c4e95cfa1f0f7ff43
lishun <silencewood@163.com>
2025-10-06 15:21:49 +0800

    feat: debugger命令

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import NextAuth from "next-auth";
-import { authConfig } from "./auth.config";
+import NextAuth from 'next-auth';
+import { authConfig } from './auth.config';
 
-export default NextAuth(authConfig).auth;
-
-export const confit = {
+const res =  NextAuth(authConfig);
+const { auth } = res
+console.log(res, "=========")
+ export default auth
+export const config = {
+  // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
   matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
-  runtime: 'nodejs'
-}
+  runtime: 'nodejs',
+};
