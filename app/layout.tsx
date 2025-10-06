c032cd667c423cbef1d84d939a149ca6722005de
lishun <silencewood@163.com>
2025-10-06 16:09:03 +0800

    feat: 设置Meta相关

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,16 @@
 import "@/app/ui/global.css";
 import { inter } from "@/app/ui/fonts";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  // title: "总览页面",
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: "next.js 仪表盘",
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+}
 
 export default function RootLayout({
   children,
